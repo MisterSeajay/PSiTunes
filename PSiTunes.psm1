52b40ec66ae6bbf17b1b577455bb5831036514b6
Charlie Joynt <cjj1977@users.noreply.github.com>
2020-06-25 02:44:42 +0100

    Move enums to separate file

--- PSiTunes.psm1
+++ PSiTunes.psm1
@@ -15,26 +15,6 @@ foreach($Folder in @($PrivateFunctions,$PublicFunctions)){
 }
 
 ###################################################################################################
-# iTunes search constants as an enum
-<#
-    ITPlaylistSearchFieldAll = 0        # Search all fields of each track.
-    ITPlaylistSearchFieldVisible = 1    # Search only the fields with columns that are currently visible in the display for the playlist. 
-                                        # Note that song name, artist, album, and composer will always be searched, even if these columns are not visible.
-    ITPlaylistSearchFieldArtists = 2    # Search only the artist field of each track (IITTrack::Artist).
-    ITPlaylistSearchFieldAlbums = 3     # Search only the album field of each track (IITTrack::Album).
-    ITPlaylistSearchFieldComposers = 4  # Search only the composer field of each track (IITTrack::Composer).
-    ITPlaylistSearchFieldSongNames = 5  # Search only the song name field of each track (IITTrack::Name).
-#>
-enum SearchTypes {
-    ITPlaylistSearchFieldAll = 0
-    ITPlaylistSearchFieldVisible = 1
-    ITPlaylistSearchFieldArtists = 2
-    ITPlaylistSearchFieldAlbums = 3
-    ITPlaylistSearchFieldComposers = 4
-    ITPlaylistSearchFieldSongNames = 5
-}
-
-###################################################################################################
 # Set paths for iTunes music and the new "shared music" location
 $GLOBAL:iTunesRoot = "D:\iTunes\iTunes Media\Music\"
 
@@ -45,3 +25,4 @@ Start-iTunes
 ###################################################################################################
 # Load the iTunes Library object
 $GLOBAL:iTunesLibrary = Get-iTunesLibrary
+$GLOBAL:iTunesMediaPath = Get-iTunesMediaLocation
