154d221ca1e47a0497676b56220f1090a167b5c5
Charlie Joynt <cjj1977@users.noreply.github.com>
2020-12-10 15:40:31 +0000

    Add additional properties from TagLib data

--- Private/getDataFromTagLib.ps1
+++ Private/getDataFromTagLib.ps1
@@ -11,10 +11,12 @@ function getDataFromTagLib {
     PROCESS {
         $TagLibFile = [TagLib.File]::Create((Resolve-Path -LiteralPath $Path))
         $DataFromTagLib =  $TagLibFile.Tag
-        $DataFromTagLib = $DataFromTagLib | Add-Member -MemberType NoteProperty -Name Location -Value $Path -PassThru
+        $DataFromTagLib = $DataFromTagLib |
+            Add-Member -MemberType NoteProperty -Name Location -Value $TagLibFile.Name -PassThru |
+            Add-Member -MemberType NoteProperty -Name MediaType -Value $TagLibFile.Properties.MediaTypes -PassThru |
+            Add-Member -MemberType NoteProperty -Name BitRate -Value $TagLibFile.Properties.AudioBitRate -PassThru
         Write-Output $DataFromTagLib
     }
 
     END {}
 }
-    
