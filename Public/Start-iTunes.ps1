76f804660c3e60a7fbebbbf3996e645bcfca1a64
Charlie Joynt <cjj1977@users.noreply.github.com>
2020-12-21 07:50:44 +0000

    Allow calling code to set global variable

--- Public/Start-iTunes.ps1
+++ Public/Start-iTunes.ps1
@@ -2,16 +2,18 @@ function Start-iTunes {
     [CmdletBinding(SupportsShouldProcess)]
     param()
     
+    $iTunesApplication = $null
+
     if($PSCmdlet.ShouldProcess("iTunes.Application","New-Object")){
         try {
-            $GLOBAL:iTunesApplication = New-Object -ComObject iTunes.Application
+            $iTunesApplication = New-Object -ComObject iTunes.Application
         }
         catch {
             if(Get-Process | ?{$_.Name -eq "iTunes"}) {
                 Write-Warning "Unable to connect to running iTunes application"
             }
-
-            $GLOBAL:iTunesApplication = $null
         }
     }
+
+    return $iTunesApplication
 }
