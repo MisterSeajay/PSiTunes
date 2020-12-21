ce9ddd4ff12c5fc4dfd457d3239521b1ae891354
Charlie Joynt <cjj1977@users.noreply.github.com>
2020-12-21 08:40:05 +0000

    Debug for failed conversions

--- Private/convertFromTagLibProperties.ps1
+++ Private/convertFromTagLibProperties.ps1
@@ -2,30 +2,37 @@ function convertFromTagLibProperties {
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipeline)]
+        [ValidateNotNullOrEmpty]
         $InputObject
     )
 
     BEGIN {}
 
     PROCESS {
-        $MusicFileInfo = [PSCustomObject]@{
-            Name = $InputObject.Title
-            Album = $InputObject.Album
-            Artist = $InputObject.FirstArtist
-            AlbumArtist = $InputObject.AlbumArtists -join ("; ")
-            Compilation = [bool]($InputObject.FirstAlbumArtist -match "various")
-            Composer = $InputObject.JoinedComposers -join ("; ")
-            Genre = $InputObject.FirstGenre
-            Grouping = $InputObject.Genres -join ("; ")
-            TrackNumber = [int]$InputObject.Track
-            TrackCount = [int]$InputObject.TrackCount
-            DiscNumber = [int]$InputObject.Disc
-            DiscCount = [int]$InputObject.DiscCount
-            Year = [int]$InputObject.Year
-            Comment = $InputObject.Comment
-            Location = $InputObject.Location
-            BitRate = $InputObject.BitRate
-        } -as [MusicFileInfo]
+        try {
+            $MusicFileInfo = [PSCustomObject]@{
+                Name = $InputObject.Title
+                Album = $InputObject.Album
+                Artist = $InputObject.FirstArtist
+                AlbumArtist = $InputObject.AlbumArtists -join ("; ")
+                Compilation = [bool]($InputObject.FirstAlbumArtist -match "various")
+                Composer = $InputObject.JoinedComposers -join ("; ")
+                Genre = $InputObject.FirstGenre
+                Grouping = $InputObject.Genres -join ("; ")
+                TrackNumber = [int]$InputObject.Track
+                TrackCount = [int]$InputObject.TrackCount
+                DiscNumber = [int]$InputObject.Disc
+                DiscCount = [int]$InputObject.DiscCount
+                Year = [int]$InputObject.Year
+                Comment = $InputObject.Comment
+                Location = $InputObject.Location
+                BitRate = $InputObject.BitRate
+            } -as [MusicFileInfo]
+        } catch {
+            Write-Warning "convertFromTagLibProperties: Failed to convert InputObject to [MusicFileInfo] type"
+            Write-Debug "convertFromTagLibProperties: $($InputObject.PSObject.Properties.Name | Out-String)"
+            Write-Error $_.Exception.Message
+        }
 
         Write-Output $MusicFileInfo
     }
