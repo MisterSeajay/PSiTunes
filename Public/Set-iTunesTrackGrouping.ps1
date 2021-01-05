fdd5b8bf36f9bb042711f86f3d6f2f8c631190c0
Charlie Joynt <cjj1977@users.noreply.github.com>
2021-01-05 15:35:14 +0000

    Use ArrayList for grouping tags

--- Public/Set-iTunesTrackGrouping.ps1
+++ Public/Set-iTunesTrackGrouping.ps1
@@ -27,38 +27,45 @@ function Set-iTunesTrackGrouping {
     PROCESS {
         # Get the current list of items in the Grouping field, or the Genre if it isn't set
         if ($Add -and ($Track.Grouping -match "\b$Add\b")) {
-            Write-Debug "NO CHANGE: Grouping already contains $Add"
+            Write-Debug "Set-iTunesTrackGrouping: Grouping already contains $Add (No Change)"
             return
 
         } elseif ($Remove -and ($Track.Grouping -notmatch "\b$Remove\b")) {
-            Write-Debug "NO CHANGE: Grouping already doesn't contain $Remove"
+            Write-Debug "Set-iTunesTrackGrouping: Grouping doesn't contain $Remove (No Change)"
             return
 
-        } elseif ($Track.Grouping){
-            $GroupingTags = ($Track.Grouping).Split(";") | ?{$_ -ne $Remove}
-
-        } else {
-            # If there isn't a Grouping already, just set it to ""
-            $GroupingTags = ""
         }
         
-        # Add the genre, splitting multi-word genres and removing non-alphabertic characters
-        $GenreTags = ($Track.Genre).Split(" ") -replace '\W',''
+        $GroupingTags = New-Object -TypeName System.Collections.ArrayList
         
-        foreach($GenreTag in $GenreTags){
-            if($Track.Grouping -notmatch "\b$GenreTag\b"){
-                Write-Debug "Adding Genre ($GenreTag) to Grouping Tags"
-                $GroupingTags+= $GenreTag
+        foreach($GroupingTag in $Track.Grouping){
+            if($GroupingTag -ne $Remove){
+                [void]$GroupingTags.Add($GroupingTag)
             }
         }
         
+        Write-Debug "Set-iTunesTrackGrouping: Existing grouping:`n$($GroupingTags | Out-String)"
+        
+        # Add the genre, splitting multi-word genres and removing non-alphabertic characters
+        $GenreTags = ($Track.Genre).Split(" ") -replace ('\W','')
+        
+        foreach($GenreTag in $GenreTags){
+            if($GenreTag -notin $GroupingTags){
+                Write-Debug "Set-iTunesTrackGrouping: Adding Genre ($GenreTag) to Grouping Tags"
+                [void]$GroupingTags.Add($GenreTag)
+            }
+        }
+
         if($Add){
-            Write-Debug "Adding $Add to Grouping Tags"
-            $GroupingTags+= $Add
+            Write-Debug "Set-iTunesTrackGrouping: Adding $Add to Grouping Tags"
+            [void]$GroupingTags.Add($Add)
         }
         
+        $NewGrouping = $GroupingTags -join ";"
+        Write-Debug "Set-iTunesTrackGrouping: New Grouping is '$NewGrouping'"
+
         # Update the track Grouping field
-        Set-iTunesTrackData -Tracks $Track -Attribute Grouping -Value ($GroupingTags -join ";")
+        Set-iTunesTrackData -Tracks $Track -Attribute Grouping -Value $NewGrouping
     }
     
     END{
