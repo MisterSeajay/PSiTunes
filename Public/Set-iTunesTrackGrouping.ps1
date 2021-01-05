c9b84f3c57c0f23bef04cb44edc64c4ff279ffdd
Charlie Joynt <cjj1977@users.noreply.github.com>
2021-01-05 17:05:12 +0000

    Simplify data sync

--- Public/Set-iTunesTrackGrouping.ps1
+++ Public/Set-iTunesTrackGrouping.ps1
@@ -4,18 +4,14 @@ function Set-iTunesTrackGrouping {
         [Parameter(
             ValueFromPipeline=$true)]
         [System.Object]
-        $Track = (Get-iTunesSelectedTracks),
+        $Tracks = (Get-iTunesSelectedTracks),
 
-        [Parameter(
-            Mandatory=$true,
-            ParameterSetName="Add")]
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
         [string]
         $Add,
 
-        [Parameter(
-            Mandatory=$true,
-            ParameterSetName="Remove")]
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
         [string]
         $Remove
@@ -25,47 +21,51 @@ function Set-iTunesTrackGrouping {
     }
     
     PROCESS {
-        # Get the current list of items in the Grouping field, or the Genre if it isn't set
-        if ($Add -and ($Track.Grouping -match "\b$Add\b")) {
-            Write-Debug "Set-iTunesTrackGrouping: Grouping already contains $Add (No Change)"
-            return
+        foreach($Track in $Tracks){
+            if($Add){
+                Write-Debug "Set-iTunesTrackGrouping: Adding: $Add"
+            }
 
-        } elseif ($Remove -and ($Track.Grouping -notmatch "\b$Remove\b")) {
-            Write-Debug "Set-iTunesTrackGrouping: Grouping doesn't contain $Remove (No Change)"
-            return
+            if($Remove){
+                Write-Debug "Set-iTunesTrackGrouping: Removing: $Remove"
+            }
+            
+            # Copy existing grouping tags into new ArrayList
+            $GroupingTags = New-Object -TypeName System.Collections.ArrayList        
+            foreach($GroupingTag in $Track.Grouping.Split(";")){
+                if($GroupingTag -ne $Remove `
+                        -and -not [string]::IsNullOrWhiteSpace($GroupingTag)){
+                    [void]$GroupingTags.Add($GroupingTag)
+                }
+            }
+            
+            # Add new tags to grouping tags
+            foreach($GroupingTag in $Add.Split(";")){
+                if($GroupingTag -notin $GroupingTags `
+                        -and -not [string]::IsNullOrWhiteSpace($GroupingTag)){
+                    [void]$GroupingTags.Add($GroupingTag)
+                }
+            }
+            
+            # Copy genre into grouping tags
+            $GenreTags = ($Track.Genre).Split(" ")
+            foreach($GenreTag in $GenreTags){
+                if($GenreTag -notin $GroupingTags `
+                        -and -not [string]::IsNullOrWhiteSpace($GenreTag)){
+                    [void]$GroupingTags.Add($GenreTag)
+                }
+            }
 
-        }
-        
-        $GroupingTags = New-Object -TypeName System.Collections.ArrayList
-        
-        foreach($GroupingTag in $Track.Grouping){
-            if($GroupingTag -ne $Remove){
-                [void]$GroupingTags.Add($GroupingTag)
+            $NewGrouping = ($GroupingTags | Sort-Object -Unique) -join ";"
+            $NewGrouping = $NewGrouping.Trim(";")
+
+            if($Track.Grouping -ne $NewGrouping){
+                # Write-Debug "Set-iTunesTrackGrouping: New Grouping is '$NewGrouping'"
+                Set-iTunesTrackData -Tracks $Track -Attribute Grouping -Value $NewGrouping
+            } else {
+                Write-Debug "Set-iTunesTrackGrouping: No change"
             }
         }
-        
-        Write-Debug "Set-iTunesTrackGrouping: Existing grouping:`n$($GroupingTags | Out-String)"
-        
-        # Add the genre, splitting multi-word genres and removing non-alphabertic characters
-        $GenreTags = ($Track.Genre).Split(" ") -replace ('\W','')
-        
-        foreach($GenreTag in $GenreTags){
-            if($GenreTag -notin $GroupingTags){
-                Write-Debug "Set-iTunesTrackGrouping: Adding Genre ($GenreTag) to Grouping Tags"
-                [void]$GroupingTags.Add($GenreTag)
-            }
-        }
-
-        if($Add){
-            Write-Debug "Set-iTunesTrackGrouping: Adding $Add to Grouping Tags"
-            [void]$GroupingTags.Add($Add)
-        }
-        
-        $NewGrouping = $GroupingTags -join ";"
-        Write-Debug "Set-iTunesTrackGrouping: New Grouping is '$NewGrouping'"
-
-        # Update the track Grouping field
-        Set-iTunesTrackData -Tracks $Track -Attribute Grouping -Value $NewGrouping
     }
     
     END{
--- Public/Sync-iTunesTrackData.ps1
+++ Public/Sync-iTunesTrackData.ps1
@@ -12,113 +12,81 @@ function Sync-iTunesTrackData {
 
         [Parameter()]
         [switch]
-        $FirstSync
+        $FirstSync,
+
+        [Parameter()]
+        [switch]
+        $Force
     )
     
     if((-not $Tracks) -or ($Tracks.Count -lt 2)){
-        Write-Warning "Minimum 2 tracks needed for sync"
+        Write-Warning "Sync-iTunesTrackData: Minimum 2 tracks needed for sync"
         return $null
-    } elseif(@($Tracks |
+    } elseif(-not $Force -and @($Tracks |
                 Select-Object Artist,@{Label="ShortName";Expression={$_.Name.ToLower() -replace "\[.+\]",""}} |
                 Sort-Object Artist,ShortName | Get-Unique -AsString).Count -gt 1){
-        Write-Warning "Name and Artist does not match for all tracks"
+        Write-Warning "Sync-iTunesTrackData: Name and Artist does not match for all tracks"
         Write-Debug ($Tracks | Select-Object -Property Name,Artist -Unique | Out-String)
         return $null
     } else {
-        Write-Verbose "$($PsCmdlet.ParameterSetName) data on $($Tracks.Count) tracks"
+        Write-Debug "Sync-iTunesTrackData: $($Tracks.Count) tracks"
     }
     
-    $PlayedDate = ($Tracks | Measure-Object -Maximum -Property PlayedDate).Maximum
-        
-    $MaxRating = [Int32]($Tracks | Measure-Object -Maximum -Property Rating).Maximum
-        
-    # NB: Unless the FirstSync switch has been used, we make an assumption that the least-
-    # played track in the list is a low-water mark, i.e. it has not been played since the last
-    # sync, so we can use that mark when calculating how many times the other tracks have been
-    # played.
-        
-    if($FirstSync){
-        $MinPlayed = 0
-    }
-    else {
-        $MinPlayed = ($Tracks | Measure-Object -Property PlayedCount -Minimum).Minimum
-    }
-          
-    # We work out how many times each track was played over this minimum count. We will also
-    # take the opportunity to add the "Sync" tag to the Grouping field so we can more-easily
-    # find tracks which have been synchronised in this way in the future and finally, we will
-    # add a "NoPlaylist" tag to remove duplicate tracks from smart playlists (for which we
-    # will make some effort to ensure the earliest version of the track is the "master").
-        
-    $PlayedCount = $MinPlayed
+    # Gather all tags from each track
+    $CombinedGroupings = $Tracks.Grouping -join ";"
+    # Strip out unwanted/reserved tags that we don't want to merge
+    $UnwantedTags = @("Female","NoPlaylist","Purchased","Re-rip","SP","Sync") -join "|"
+    $CombinedGroupings = $CombinedGroupings -replace ("\b($UnwantedTags)\b","")
+    # Clean up string
+    $CombinedGroupings = $CombinedGroupings -replace (";{2,}",";")
+    $CombinedGroupings = $CombinedGroupings.Trim(";")
+
     $AddNoPlaylist = 0
     
-    $CombinedGroupings = $Tracks.Grouping -join ";"
-    $CombinedGroupings+= ";Sync" # tag for future re-syncs
+    if($FirstSync){
+        $PlayedCount = [Int32]($Tracks | Measure-Object -Property PlayedCount -Sum).Sum
+    } else {
+        $PlayedCount = [Int32]($Tracks | Measure-Object -Property PlayedCount -Maximum).Maximum
+    }
 
+    if($PlayedCount -eq 0){
+        $PlayedDate = [DateTime]"1899-12-30"
+    } else {
+        $PlayedDate = [DateTime]($Tracks | Measure-Object -Maximum -Property PlayedDate).Maximum
+    }
+
+    $MaxRating = [Int32]($Tracks | Measure-Object -Maximum -Property Rating).Maximum
+            
     foreach($Track in ($Tracks | Sort-Object Compilation,Year,Album)){
 
-        ###########################################################################################
-        # Work out total PlayedCount: Add the number of additional times this track has been played
+        Write-Verbose "Sync-iTunesTrackData: Updating GROUPING for $(formatiTunesTrackInfo -Track $Track)"
 
-        if($Track.PlayedCount -gt $MinPlayed){
-            $PlayedCount+= ($Track.PlayedCount - $MinPlayed)
-        }
-            
         ###########################################################################################
         # Merge the grouping tags to each track
-
-        Set-iTunesTrackGrouping -Track $Track -Add $CombinedGroupings
-            
-        ###########################################################################################
-        # Ensure that only one version of this song is added to smart playlists
-        # 
-        # If the AddNoPlaylist switch has been set (after the first run of this loop) we add
-        # that tag, else we ensure that the tag is removed in case it was set previously.
+        #
+        # If the AddNoPlaylist switch has been set (after the first run of this
+        # loop) we add that tag, else we ensure that the tag is removed in case
+        # it was set previously.
+        #
+        # After the first run through the list of tracks we set this flag to
+        # ensure the rest of the list get the "NoPlaylist" tag added in their
+        # grouping field.
             
         if($AddNoPlaylist){
-            Set-iTunesTrackGrouping -Track $Track -Add "NoPlaylist"
+            Set-iTunesTrackGrouping -Track $Track -Add "$CombinedGroupings;NoPlaylist;Sync"
         } else {
-            Set-iTunesTrackGrouping -Track $Track -Remove "NoPlaylist"
+            Set-iTunesTrackGrouping -Track $Track -Add "$CombinedGroupings;Sync" -Remove "NoPlaylist"
         }
-            
-        # After the first run through the list of tracks we set this flag to ensure the rest
-        # of the list get the "NoPlaylist" tag added in their grouping field.
             
         $AddNoPlaylist = 1
-    }
-        
-    if($PlayedCount -eq 0){
-        $PlayedDate = "1899-12-30"
-    }
-    
-    #################################################################################################
-    # Synchronize track data
 
-    foreach($Track in $Tracks){
+        #######################################################################
+        # Update other Metadata
 
-        $PlayedCount = [Int]$PlayedCount
-        try {
-            $Track | Set-iTunesTrackData -Attribute PlayedCount -Value $PlayedCount
-        }
-        catch {
-            throw
-        }
+        $Track | Set-iTunesTrackData -Attribute PlayedCount -Value $PlayedCount
 
-        $PlayedDate = [DateTime]$PlayedDate
-        try {
-            $Track | Set-iTunesTrackData -Attribute PlayedDate -Value $PlayedDate
-        }
-        catch {
-            throw
-        }
+        $Track | Set-iTunesTrackData -Attribute PlayedDate -Value $PlayedDate
 
-        $MaxRating = [Int]$MaxRating
-        try {
-            $Track | Set-iTunesTrackData -Attribute Rating -Value $MaxRating
-        }
-        catch {
-            throw
-        }
+        $Track | Set-iTunesTrackData -Attribute Rating -Value $MaxRating
     }
 }
