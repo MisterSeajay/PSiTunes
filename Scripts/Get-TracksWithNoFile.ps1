a93aa1e163cb3e4bd9908bd4ee2883e8695af314
Charlie Joynt <cjj1977@users.noreply.github.com>
2022-06-22 15:20:29 +0100

    improve

--- Scripts/Get-TracksWithNoFile.ps1
+++ Scripts/Get-TracksWithNoFile.ps1
@@ -1,12 +1,30 @@
+param(
+    $BasePath = "D:\iTunes\iTunes Media\Music\",
+    $Limit = $null
+)
+
 $cwd = Split-Path $MyInvocation.InvocationName -Parent
 $PSiTunes = Resolve-Path -Path (Join-Path $cwd "../PSiTunes.psd1")
 Import-Module $PSiTunes -Force -Verbose:$false
 
-$itunesLibrary.Tracks |
+$MissingLocation = $itunesLibrary.Tracks |
     Where-Object {[string]::IsNullOrEmpty($_.Location) `
         -and -not [string]::IsNullOrEmpty($_.AlbumArtist) `
-        -and $_.Genre -notin ("Classical", "Comedy", "Guitar", "Karaoke")} |
-    Group-Object -Property AlbumArtist, Album |
-    Where-Object {$_.Count -gt 5} |
-    Sort-Object -Property Name |
-    Select-Object -First 20 -Property Name, Count
+        -and $_.Genre -notin ("Classical", "Comedy", "Guitar", "Karaoke")}
+
+if($Limit){
+    $MissingLocation = $MissingLocation | Select-Object -First $Limit
+}
+
+foreach($Track in $MissingLocation) {
+    $ExpectedLocation = $BasePath
+
+    if($Track.AlbumArtist -eq "VariousArtists"){
+        $ExpectedLocation += "Compilations\$($Track.Album)\$($Track.DiscNumber)-$($Track.TrackNumber) - $($Track.Artist) - $($Track.Name)"
+    } else {
+        $ExpectedLocation += "$($Track.AlbumArtist)\$($Track.Album)\$($Track.DiscNumber)-$($Track.TrackNumber) - $($Track.Name)"
+    }
+
+    Write-Debug $ExpectedLocation
+    Test-Path -LiteralPath $ExpectedLocation
+}
